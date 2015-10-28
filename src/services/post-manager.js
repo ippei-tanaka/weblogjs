3bc17f9293292cc9b9815f3e69c2dbae97cab2ec
Ippei Tanaka <itanaka@unioncreative.com>
2015-10-28 00:59:58 -0400

    Updated model managers

--- src/services/post-manager.js
+++ src/services/post-manager.js
@@ -1,34 +1,54 @@
 "use strict";
 
 
+var categoryManager = require('./category-manager');
 var Post = require('../models/post');
 var errors = require('../errors');
 
+// Regular expression that checks for hex value
+var checkForHexRegExp = new RegExp("^[0-9a-fA-F]{24}$");
 
 /**
  * @param {object} postInfo - Information about the post.
  * @param {string} postInfo.title - The title of the post.
  * @param {string} postInfo.content - The content of the post.
  * @param {string} postInfo.author - The author ID of the post.
- * @param {string} [postInfo.category] - The category ID of the post.
+ * @param {string} [postInfo.category] - The category slug or ID of the post.
  * @param {string} [postInfo.slug] - The slug of the post.
  * @param {[string]} [postInfo.tags] - The tags of the post.
  * @returns {Promise}
  */
 var create = (postInfo) => new Promise((resolve, reject) => {
-    var post = new Post({
-        title: postInfo.title,
-        content: postInfo.content,
-        author: postInfo.author,
-        category: postInfo.category,
-        slug: postInfo.slug,
-        tags: postInfo.tags
-    });
 
-    post.save((err) => {
-        if (err) return reject(err);
-        resolve(post.toJSON());
-    });
+    var promise,
+        categoryValue = String(postInfo.category);
+
+    if (!postInfo.category) {
+        promise = Promise.resolve(null);
+    } else if (checkForHexRegExp.test(categoryValue)) {
+        promise = categoryManager.findById(categoryValue);
+    } else {
+        promise = categoryManager.findBySlug(categoryValue);
+    }
+
+    promise
+        .then((category) => {
+            var post = new Post({
+                title: postInfo.title,
+                content: postInfo.content,
+                author: postInfo.author,
+                category: category ? category.id : undefined,
+                slug: postInfo.slug,
+                tags: postInfo.tags
+            });
+
+            post.save((err) => {
+                if (err) return reject(err);
+                resolve(post.toJSON());
+            });
+        })
+        .catch(reject);
+
 });
 
 
