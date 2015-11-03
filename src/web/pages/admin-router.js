d3376141625c72997b2c564599be7b35d4aae318
Ippei Tanaka <tanaka.ippei.jp@gmail.com>
2015-11-03 14:11:50 -0500

    Fixed auth

--- src/web/pages/admin-router.js
+++ src/web/pages/admin-router.js
@@ -6,18 +6,31 @@ var url = require('url');
 var localAuth = require('../passport-manager').localAuth;
 
 
+var isLoggedIn = (req, res, next) => {
+    if (req.isAuthenticated())
+        return next();
+
+    res.sendStatus(401);
+};
+
 // Home
 
-routes.get('/', localAuth, (request, response) => {
+routes.get('/', isLoggedIn, (request, response) => {
     response.render('admin-home');
 });
 
 routes.get('/login', (request, response) => {
+    response.redirect('/admin/');
+});
+
+
+routes.post('/login', localAuth, (request, response) => {
     response.render('login');
 });
 
-routes.post('/login', localAuth, (request, response) => {
-    response.redirect('/');
+
+routes.get('/test', (request, response) => {
+    response.render('test');
 });
 
 module.exports = {
