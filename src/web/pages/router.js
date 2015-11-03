25b781c6622b299e728b0a8ac88ce726cb2ce0f0
Ippei Tanaka <itanaka@unioncreative.com>
2015-11-03 10:26:01 -0500

    Set up mustache view engine and bootstrap

---
+++ src/web/pages/router.js
@@ -0,0 +1,32 @@
+"use strict";
+
+
+var routes = require('express').Router();
+var url = require('url');
+var api = require('../../api');
+var userManager = api.userManager;
+var categoryManager = api.categoryManager;
+var postManager = api.postManager;
+var auth = require('../passport-manager').basicAuth;
+
+
+// Home
+
+routes.get('/',  (request, response) => {
+    response.render('master', {
+        head: {
+            title: 'page title'
+        },
+        content: {
+            title: 'post title',
+            description: 'description'
+        }
+    });
+});
+
+
+
+module.exports = {
+    routes
+};
+
