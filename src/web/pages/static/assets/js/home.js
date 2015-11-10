e4dd025019ae509b5abcd07bb484906582039863
Ippei Tanaka <tanaka.ippei.jp@gmail.com>
2015-11-10 16:31:21 -0500

    Updated style

--- src/web/pages/static/assets/js/home.js
+++ src/web/pages/static/assets/js/home.js
@@ -1,6 +1,11 @@
 requirejs([
-        'jsx!components/user-list'
+        'jsx!components/user-list',
+        'jsx!components/user-creator'
     ],
-    function (UserList) {
+    function (
+        UserList,
+        UserCreator
+    ) {
         UserList.render(document.querySelector("[data-react='user-list']"));
+        UserCreator.render(document.querySelector("[data-react='user-creator']"));
     });
