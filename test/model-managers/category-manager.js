4f41b0c9a496bf3bbcc714315aa85ecede546bd1
Ippei Tanaka <tanaka.ippei.jp@gmail.com>
2015-11-25 18:22:04 -0500

    Added model-manager test directory

---
+++ test/model-managers/category-manager.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var config = require('../config.json');
+var weblogjs = require('../../')(config);
+var expect = require('chai').expect;
+
+
+describe('Category Manager', () => {
+
+    it('should do something', (done) => {
+        done();
+    });
+
+});
