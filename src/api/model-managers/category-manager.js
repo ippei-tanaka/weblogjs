c651ec9f34b3c7191ab4c66630ebfa2c521f747e
Ippei Tanaka <tanaka.ippei.jp@gmail.com>
2015-11-26 12:19:01 -0500

    Updated tests

--- src/api/model-managers/category-manager.js
+++ src/api/model-managers/category-manager.js
@@ -8,54 +8,8 @@ var errors = require('../errors/index');
 var exports = ___modelManager.applyTo(Category);
 
 
-var create = exports.create;
+exports.findOneBySlug = function (slug) {
+    return this.findOne({slug: slug});
+}.bind(exports);
 
-
-var getList = exports.getList;
-
-/**
- * @param {string} id
- * @returns {Promise}
- */
-var findById = modelManager.findById.bind({}, Category, null);
-
-
-/**
- * @param {string} slug
- * @returns {Promise}
- */
-var findBySlug = (slug) => {
-    return modelManager.findOneBy(Category, "slug", slug);
-};
-
-
-/**
- * @param {string} id
- * @param {object} newCategory
- * @param {string} [newCategory.name] - The name of the category.
- * @param {string} [newCategory.slug] - The content of the category.
- * @returns {Promise}
- */
-var updateById = (id, newCategory) => {
-    return modelManager.updateById(Category, id, {
-        name: newCategory.name,
-        slug: newCategory.slug
-    });
-};
-
-
-/**
- * @param {string} id
- * @returns {Promise}
- */
-var removeById = modelManager.removeById.bind({}, Category);
-
-
-module.exports = {
-    create,
-    getList,
-    findById,
-    findBySlug,
-    updateById,
-    removeById
-};
+module.exports = exports;
--- test/model-managers/category-manager.js
+++ test/model-managers/category-manager.js
@@ -37,7 +37,7 @@ describe('Category Manager', () => {
 
     it('should return a filtered, sorted list of categories', (done) => {
         co(function* () {
-            var randomString = function (length) {
+            let randomString = function (length) {
                 var charList = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
                 var text = "";
 
@@ -54,21 +54,21 @@ describe('Category Manager', () => {
                 })
             }
 
-            let categories = yield weblogjs.api.categoryManager.getList({
+            let categories = yield weblogjs.api.categoryManager.find({}, {
                 sort: "slug desc"
             });
             expect(categories.length).to.equal(10);
             expect(categories[0].slug).to.equal("this-is-a-category-9");
             expect(categories[9].slug).to.equal("this-is-a-category-0");
 
-            categories = yield weblogjs.api.categoryManager.getList({
+            categories = yield weblogjs.api.categoryManager.find({}, {
                 sort: "slug asc"
             });
             expect(categories.length).to.equal(10);
             expect(categories[0].slug).to.equal("this-is-a-category-0");
             expect(categories[9].slug).to.equal("this-is-a-category-9");
 
-            categories = yield weblogjs.api.categoryManager.getList({
+            categories = yield weblogjs.api.categoryManager.find({}, {
                 sort: "slug asc",
                 offset: 2,
                 limit: 6
@@ -76,6 +76,74 @@ describe('Category Manager', () => {
             expect(categories.length).to.equal(6);
             expect(categories[0].slug).to.equal("this-is-a-category-2");
             expect(categories[5].slug).to.equal("this-is-a-category-7");
+
+            done();
+        }).catch((error) => {
+            console.error(error);
+        })
+    });
+
+    it('should return a category or categories', (done) => {
+        co(function* () {
+            let category1 = yield weblogjs.api.categoryManager.create({
+                name: "This is a category 1",
+                slug: `this-is-a-category-1`
+            });
+
+            yield weblogjs.api.categoryManager.create({
+                name: "This is a category 2",
+                slug: `this-is-a-category-2`
+            });
+
+            yield weblogjs.api.categoryManager.create({
+                name: "This is a category 2",
+                slug: `this-is-a-category-2-2`
+            });
+
+
+            let _category = yield weblogjs.api.categoryManager.find({
+                name: "This is a category 2"
+            });
+
+            expect(_category[0].name).to.equal("This is a category 2");
+            expect(_category[1].name).to.equal("This is a category 2");
+
+            _category = yield weblogjs.api.categoryManager.findOne({
+                name: "This is a category 1"
+            });
+
+            expect(_category.name).to.equal("This is a category 1");
+            expect(_category.slug).to.equal("this-is-a-category-1");
+
+             _category = yield weblogjs.api.categoryManager.findOneById(category1.id);
+
+            expect(_category.name).to.equal("This is a category 1");
+            expect(_category.slug).to.equal("this-is-a-category-1");
+
+            _category = yield weblogjs.api.categoryManager.findOneBySlug("this-is-a-category-2");
+
+            expect(_category.name).to.equal("This is a category 2");
+            expect(_category.slug).to.equal("this-is-a-category-2");
+
+            done();
+        }).catch((error) => {
+            console.error(error);
+        })
+    });
+
+    it('should update a category', (done) => {
+        co(function* () {
+            let category = yield weblogjs.api.categoryManager.create({
+                name: "This is a category",
+                slug: `this-is-a-category`
+            });
+
+            let _categories = yield weblogjs.api.categoryManager.update({_id: category.id}, {
+                name: "This is a new category!"
+            });
+
+            expect(_categories[0].name).to.equal("This is a new category!");
+            expect(_categories[0].slug).to.equal("this-is-a-category");
 
             done();
         }).catch((error) => {
