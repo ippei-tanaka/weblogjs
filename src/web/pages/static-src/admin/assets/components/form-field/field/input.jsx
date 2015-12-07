e0c15439d3258863fd1a070bb5e96507bb991f88
Ippei Tanaka <itanaka@unioncreative.com>
2015-12-07 13:52:55 -0500

    Experiment inheritance

--- src/web/pages/static-src/admin/assets/components/form-field/field/input.jsx
+++ src/web/pages/static-src/admin/assets/components/form-field/field/input.jsx
@@ -1,51 +1,6 @@
-import React from 'react';
+import Field from './field';
 
-class Input extends React.Component {
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            value: ""
-        };
-    }
-
-    componentWillMount() {
-        this._props = Object.assign({}, this.props);
-        this._props.onChange = this._onChange.bind(this);
-    }
-
-    componentWillReceiveProps(newProps) {
-        this.setState({
-            value: newProps.value
-        });
-    }
-
-    render() {
-        this._props.value = this.state.value;
-        return React.createElement("input", this._props);
-    }
-
-    _onChange(e) {
-        var value = e.target.value;
-
-        if (this.props.onChange(value) === false) {
-            e.preventDefault();
-            return;
-        }
-
-        this.setState({
-            value: value
-        });
-    }
-
+class Input extends Field {
 }
-
-Input.defaultProps = {
-    className: "m-frf-text",
-    value: "",
-    onChange: function () {
-    }
-};
 
 export default Input;
