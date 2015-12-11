c0d83694d1a84deb52dc2d222713172a5cc1a8ce
Ippei Tanaka <tanaka.ippei.jp@gmail.com>
2015-12-11 09:01:31 -0500

    Refactored

--- src/web/pages/views/helpers/index.js
+++ src/web/pages/views/helpers/index.js
@@ -1,8 +1,15 @@
 "use strict";
 
+var moment = require('moment');
+
+
 module.exports = {
     'blog_content': function(text, options) {
         var ret = text.replace(/\n/g, '<br />');
         return ret;
+    },
+
+    'date_format': function(date, options) {
+        return moment(date).format("MMM D, YYYY");
     }
 };
