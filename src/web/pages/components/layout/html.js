a1960e4e4cb3cd8208fecfdada55f8f22bddb43e
Ippei Tanaka <itanaka@unioncreative.com>
2016-01-04 19:36:05 -0500

    Updated webpack setting

--- src/web/pages/components/layout/html.js
+++ src/web/pages/components/layout/html.js
@@ -12,7 +12,7 @@ export default class Html extends React.Component {
                 <title></title>
             </head>
             <body>
-            <div id="App">
+            <div id="App" className="module-app">
                 [CONTENT_PLACE_HOLDER]
             </div>
             <script src="http://localhost:8080/index.js"></script>
