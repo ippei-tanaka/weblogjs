a94820172967efd4a52d75c8cb6889164b616e71
Ippei Tanaka <tanaka.ippei.jp@gmail.com>
2016-01-05 09:52:49 -0500

    Moved categories

--- src/web/pages/components/form/button/button-list.jsx
+++ src/web/pages/components/form/button/button-list.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import Button from './button';
-import SubmitButton from './submit-button';
 
 
 export default function ButtonList ({children}) {
@@ -8,11 +6,6 @@ export default function ButtonList ({children}) {
         <div className="m-dte-field-container">
             <ul className="m-dte-button-list">
                 {React.Children.map(children, child => {
-
-                    if (child.type !== Button
-                        && child.type !== SubmitButton)
-                        throw new Error("Each cild has to be Button.");
-
                     return (
                     <li className="m-dte-button-list-item">
                         {child}
