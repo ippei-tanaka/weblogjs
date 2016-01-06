cdf30f1a4fb55b1a47fac33d0de8e5666ee04298
Ippei Tanaka <tanaka.ippei.jp@gmail.com>
2016-01-06 18:18:40 -0500

    Refactoed Deleter

--- src/web/pages/components/app/admin/category/category-deleter.js
+++ src/web/pages/components/app/admin/category/category-deleter.js
@@ -1,33 +1,54 @@
-import Deleter from '../../../abstructs/deleter';
+import React from 'react';
 import ServerFacade from '../../../../services/server-facade';
+///import UserStore from '../../../../stores/user-store';
+import Page from '../../../abstructs/page';
+import Deleter from '../../../partials/deleter';
 
 
-class CategoryDeleter extends Deleter {
+class CategoryDeleter extends Page {
 
-    retrieveLabel(id) {
-        return ServerFacade.getCategory(id).then(category => {
-            return category.name;
-        });
+    constructor(props) {
+        super(props);
+        this.state = {
+            label: ""
+        };
     }
 
-    deleteModel(id) {
-        return ServerFacade.deleteCategory(id);
+    componentWillMount() {
+        this.updateLabel();
+    }
+
+    render() {
+        return (
+            <Deleter title={this.title}
+                     label={this.state.label}
+                     onApproved={() => this.deleteModel()}
+                     onCanceled={() => this.goToListPage()}/>
+        );
+    }
+
+    updateLabel() {
+        ServerFacade
+            .getCategory(this.props.params.id)
+            .then(category => {
+                return category.name;
+            })
+            .then((values) => this.setState({label: values}))
+            .catch(data => console.error(data));
+    }
+
+    deleteModel() {
+        ServerFacade
+            .deleteCategory(this.props.params.id)
+            .then(() => this.goToListPage());
+    }
+
+    goToListPage() {
+        this.context.history.pushState(null, "/admin/categories");
     }
 
     get title() {
-        return "Delete the Category";
-    }
-
-    get pageTitle() {
-        return "Delete the Category";
-    }
-
-    buildSuccessLocation(id) {
-        return "/admin/categories"
-    }
-
-    buildCancelLocation(id) {
-        return "/admin/categories"
+        return "Delete Category";
     }
 
 }
