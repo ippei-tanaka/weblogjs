d78bf43249a763ba0438c344e6edfb54d5aaa14f
Ippei Tanaka <itanaka@unioncreative.com>
2016-01-18 17:38:44 -0500

    Created admin-menu

---
+++ src/web/pages/components/app/admin/partials/admin-menu.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link, IndexLink } from 'react-router';
+
+var AdminMenu = ({onLogoutClick}) => (
+    <menu>
+        <li>
+            <button onClick={onLogoutClick}>Log out</button>
+        </li>
+        <li><IndexLink to="/admin">Dashboard</IndexLink></li>
+        <li><Link to="/admin/users">Users</Link></li>
+        <li><Link to="/admin/categories">Categories</Link></li>
+        <li><Link to="/admin/blogs">Blogs</Link></li>
+        <li><Link to="/admin/posts">Posts</Link></li>
+        <li><Link to="/admin/setting">Setting</Link></li>
+    </menu>
+);
+
+export default AdminMenu;
