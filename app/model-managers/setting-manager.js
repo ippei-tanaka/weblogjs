a180a6630d761a1f960d5235b51f7be699552fb8
Ippei Tanaka <tanaka.ippei.jp@gmail.com>
2016-01-20 13:29:55 -0500

    Fixed setting-manager

--- app/model-managers/setting-manager.js
+++ app/model-managers/setting-manager.js
@@ -6,20 +6,24 @@ import co from 'co';
 var settingManager = new ModelManager(Setting);
 
 
+var getSetting = () => co(function* () {
+    var docs = yield settingManager.find({});
+    return docs.length >= 1 ? docs[0] : new Setting();
+});
+
+
+var setFront = blogId => co(function* () {
+    var setting = yield getSetting();
+
+    setting.front = blogId;
+
+    yield setting.save();
+
+    return yield getSetting();
+});
+
+
 export default {
-
-    getSetting: () => co(function* () {
-        var docs = yield settingManager.find();
-        return docs.length >= 1 ? docs[0] : new Setting();
-    }),
-
-    setFront: (blogId) => co(function* () {
-        var setting = yield this.getSetting();
-
-        setting.front = blogId;
-
-        yield setting.save();
-
-        return yield this.getSetting();
-    }.bind(exports))
+    getSetting,
+    setFront
 };
