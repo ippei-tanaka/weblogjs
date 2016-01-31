471963e315f759e79b16b1d8745f37cd5687e1a0
Ippei Tanaka <itanaka@unioncreative.com>
2016-01-31 17:00:10 -0500

    Added DevTools

--- app/web-pages/js/stores/app-store.js
+++ app/web-pages/js/stores/app-store.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import promiseMiddleWare from '../middlewares/promise-middleware';
 import functionMiddleWare from '../middlewares/function-middleware';
+import DevTools from '../containers/dev-tools';
 import userReducer from '../reducers/user-reducer';
 import authReducer from '../reducers/auth-reducer';
 
@@ -10,6 +11,17 @@ const reducer = combineReducers({
     auth: authReducer
 });
 
-const finalCreateStore = applyMiddleware(functionMiddleWare, promiseMiddleWare)(createStore);
+const enhancer = compose(
+    // Middleware you want to use in development:
+    applyMiddleware(functionMiddleWare, promiseMiddleWare),
+
+    // Required! Enable Redux DevTools with the monitors you chose
+    DevTools.instrument()//,
+
+    // Optional. Lets you write ?debug_session=<key> in address bar to persist debug sessions
+    //persistState(getDebugSessionKey())
+);
+
+const finalCreateStore = enhancer(createStore);
 
 export default finalCreateStore(reducer);
