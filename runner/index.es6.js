85f1244f1c442dc1c3b97b8422de77a373654214
Ippei Tanaka <itanaka@unioncreative.com>
2016-05-25 14:07:35 -0400

    Renamed example/app to runner and fix it

---
+++ runner/index.es6.js
@@ -0,0 +1,31 @@
+import configFile from './config.json';
+import WeblogJS from '../app';
+import co from 'co';
+
+WeblogJS.init({
+    dbHost: configFile.database_host,
+    dbPort: configFile.database_port,
+    dbName: configFile.database_name,
+    webHost: configFile.web_server_host,
+    webPort: configFile.web_server_port,
+    apiRoot: configFile.restful_api_root,
+    sessionSecret: configFile.session_secret
+});
+
+const admin = Object.freeze({
+    email: configFile.admin_email,
+    password: configFile.admin_password,
+    display_name: "Admin",
+    slug: 'admin'
+});
+
+co(function* () {
+    yield WeblogJS.webServer.start();
+    yield WeblogJS.dbSettingOperator.dropDatabase();
+    yield WeblogJS.dbSettingOperator.createIndexes();
+    yield WeblogJS.dbSettingOperator.removeAllDocuments();
+    yield WeblogJS.createUser(admin);
+    console.log("Web Server has started...");
+}).catch((error) => {
+    console.error(error);
+});
