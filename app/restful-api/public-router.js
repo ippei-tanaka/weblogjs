042eec8d1cbf14724c70313443a960ad14488085
Ippei Tanaka <itanaka@unioncreative.com>
2016-05-29 19:15:26 -0400

    Remove comment

--- app/restful-api/public-router.js
+++ app/restful-api/public-router.js
@@ -5,40 +5,6 @@ import PassportManager from '../passport-manager';
 import Models from '../models';
 import { successHandler, errorHandler, parseParameters } from './lib/handlers';
 
-/*
-const addRoutesForCrudOperations = (schemaName, router) => {
-
-    const Model = Models.getModel(schemaName);
-    const path = pluralize(schemaName);
-
-    router.get(`/${path}`, (request, response) => co(function* () {
-        const { query, sort, limit, skip } = parseParameters(request.url);
-        const models = yield Model.findMany({query, sort, limit, skip});
-        successHandler(response, {items: models});
-    }).catch(errorHandler(response)));
-
-    router.get(`/${path}/:id`, (request, response) => co(function* () {
-        const model = yield Model.findOne({_id: request.params.id});
-        successHandler(response, model);
-    }).catch(errorHandler(response)));
-
-    return router;
-};
-
-const addRoutesForSetting = (router) => {
-
-    const SettingModel = Models.getModel('setting');
-
-    router.get(`/setting`, (request, response) => co(function* () {
-        const model = yield SettingModel.getSetting();
-        successHandler(response, model);
-    }).catch(errorHandler(response)));
-
-    return router;
-};
-*/
-
-
 const parseParam = (str, defaultValue) => {
     const arr = str ? str.split('/') : [];
     return arr.length >= 3 && arr[2] ? arr[2] : defaultValue;
