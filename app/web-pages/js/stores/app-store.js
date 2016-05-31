e63eafbfbf8e0a68176e383469ca14f833027523
Ippei Tanaka <tanaka.ippei.jp@gmail.com>
2016-05-31 13:16:55 -0400

    Refactor store

--- app/web-pages/js/stores/app-store.js
+++ app/web-pages/js/stores/app-store.js
@@ -1,37 +1,4 @@
-import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
-import promiseMiddleWare from '../middlewares/promise-middleware';
-import functionMiddleWare from '../middlewares/function-middleware';
-import userReducer from '../reducers/user-reducer';
-import categoryReducer from '../reducers/category-reducer';
-import blogReducer from '../reducers/blog-reducer';
-import postReducer from '../reducers/post-reducer';
-import settingReducer from '../reducers/setting-reducer';
-import authReducer from '../reducers/auth-reducer';
-import transactionReducer from '../reducers/transaction-reducer';
-import publicPageReducer from '../reducers/public-page-reducer';
-//import DevTools from '../containers/dev-tools';
+import createStore from './create-store';
+import reducers from '../reducers';
 
-const reducer = combineReducers({
-    user: userReducer,
-    category: categoryReducer,
-    blog: blogReducer,
-    post: postReducer,
-    setting: settingReducer,
-    auth: authReducer,
-    transaction: transactionReducer,
-    publicPage: publicPageReducer
-});
-
-const enhancers = [applyMiddleware(functionMiddleWare, promiseMiddleWare)];
-
-/*
-if (DEVELOPMENT_MODE) {
-    enhancers.push(DevTools.instrument());
-}
-*/
-
-const enhancer = compose.apply(compose, enhancers);
-
-const finalCreateStore = enhancer(createStore);
-
-export default finalCreateStore(reducer);
+export default createStore(reducers);
