973bfd3c8c7af21695adf9721e1cc2161220f060
Ippei Tanaka <itanaka@unioncreative.com>
2016-06-07 12:32:57 -0400

    Move browser entry files and layout files

--- app/web-pages/browser-entry/admin-browser.js
+++ app/web-pages/browser-entry/admin-browser.js
@@ -2,22 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Router, browserHistory } from 'react-router'
 import { Provider } from 'react-redux';
-import reducers from './js/reducers';
-import createStore from './js/stores/create-store';
-import createActions from './js/stores/create-actions';
-import publicRoutes from './js/routers/public-routes';
+import reducers from '../js/reducers';
+import createStore from './../js/stores/create-store';
+import adminRoutes from './../js/routers/admin-routes';
 
-require('./sass/public/main.scss');
+require('./../sass/admin/main.scss');
 
 const preloadedState = window.__PRELOADED_STATE__;
 const store = createStore(reducers, preloadedState);
-const actions = createActions(store);
 
 document.addEventListener("DOMContentLoaded", () =>
     ReactDOM.render(
         <Provider store={store}>
             <Router history={browserHistory}>
-                {publicRoutes({store, actions})}
+                {adminRoutes()}
             </Router>
         </Provider>,
         document.getElementById('AppContainer')
--- app/web-pages/browser-entry/public-browser.js
+++ app/web-pages/browser-entry/public-browser.js
@@ -2,20 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Router, browserHistory } from 'react-router'
 import { Provider } from 'react-redux';
-import reducers from './js/reducers';
-import createStore from './js/stores/create-store';
-import adminRoutes from './js/routers/admin-routes';
+import reducers from '../js/reducers';
+import createStore from '../js/stores/create-store';
+import createActions from '../js/stores/create-actions';
+import publicRoutes from '../js/routers/public-routes';
 
-require('./sass/admin/main.scss');
+require('./../sass/public/main.scss');
 
 const preloadedState = window.__PRELOADED_STATE__;
 const store = createStore(reducers, preloadedState);
+const actions = createActions(store);
 
 document.addEventListener("DOMContentLoaded", () =>
     ReactDOM.render(
         <Provider store={store}>
             <Router history={browserHistory}>
-                {adminRoutes()}
+                {publicRoutes({store, actions})}
             </Router>
         </Provider>,
         document.getElementById('AppContainer')
