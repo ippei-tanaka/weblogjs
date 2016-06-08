1f8b2e94089000187b0d8815aa4e945c0d2d5452
Ippei Tanaka <tanaka.ippei.jp@gmail.com>
2016-06-08 17:59:10 -0400

    Update constants/config.js

--- src/app/web-pages/web-app/constants/config.js
+++ src/app/web-pages/web-app/constants/config.js
@@ -5,5 +5,5 @@ const WEBPACK_MODE = ENV.mode === 'webpack';
 export const ADMIN_DIR = '/admin';
 export const PUBLIC_API_PATH = ENV.public_api_root;
 export const ADMIN_API_PATH = ENV.admin_api_root;
-export const URL_BASE = WEBPACK_MODE ? "" : `http://localhost:${ENV.web_port}`;
+export const URL_BASE = WEBPACK_MODE ? "" : `//${ENV.web_host}:${ENV.web_port}`;
 export const PostsPerPageList = Object.freeze([1, 2, 3, 5, 10, 15]);
