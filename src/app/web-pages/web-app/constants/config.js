340c7ca13262d6d9dae6abb3099e2b05cee2b933
Ippei Tanaka <tanaka.ippei.jp@gmail.com>
2016-06-08 18:39:18 -0400

    Fix a bug of constants/config

--- src/app/web-pages/web-app/constants/config.js
+++ src/app/web-pages/web-app/constants/config.js
@@ -1,9 +1,9 @@
 import ENV from './env';
 
-const WEBPACK_MODE = ENV.mode === 'webpack';
+const WEBPACK_MODE = ENV.mode === 'webpack' || ENV.mode === 'webpack-dev-server';
 
 export const ADMIN_DIR = '/admin';
 export const PUBLIC_API_PATH = ENV.public_api_root;
 export const ADMIN_API_PATH = ENV.admin_api_root;
-export const URL_BASE = WEBPACK_MODE ? "" : `//${ENV.web_host}:${ENV.web_port}`;
+export const URL_BASE = WEBPACK_MODE ? "" : `http://${ENV.web_host}:${ENV.web_port}`;
 export const PostsPerPageList = Object.freeze([1, 2, 3, 5, 10, 15]);
