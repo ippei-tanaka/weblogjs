c901170736e088f7ed207fe25a0020f06c87a1ea
Ippei Tanaka <itanaka@unioncreative.com>
2016-06-30 15:34:20 -0400

    Update error-message component

--- src/app/web-pages/web-app/components/form/decorators/error-message.js
+++ src/app/web-pages/web-app/components/form/decorators/error-message.js
@@ -4,7 +4,7 @@ const ErrorMessage = ({error = []}) => error.length > 0 && (
     <ul>
         {error.map((error, index) =>
             <li className="module-field-error-message" key={index}>
-                {error.message}
+                {error}
             </li>
         )}
     </ul>
