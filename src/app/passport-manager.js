3acf52cc34a3a210cee269ee099d9b8fb14a4372
Ippei Tanaka <itanaka@unioncreative.com>
2016-07-04 12:32:34 -0400

    Refactor passport-manager

--- src/app/passport-manager.js
+++ src/app/passport-manager.js
@@ -4,65 +4,58 @@ import co from 'co';
 import UserModel from './models/user-model';
 import { ObjectID } from 'mongodb';
 
-let localAuth = passport.authenticate('local');
+const localAuth = passport.authenticate('local');
 
-let instance = null;
+const authHandler = (email, password, done) => co(function* ()
+{
+    const model = yield UserModel.findOne({email});
 
-class PassportManager {
-
-    constructor () {
-        if (instance) {
-            return;
-        }
-        instance = this;
-
-        passport.use(new LocalStrategy({usernameField: 'email'}, this._authHandler.bind(this)));
-
-        passport.serializeUser((user, done) => {
-            done(null, user._id);
-        });
-
-        passport.deserializeUser(this._deserializeUser);
+    if (!model)
+    {
+        return done();
     }
 
-    get passport () {
+    if (!(yield model.checkPassword(password)))
+    {
+        return done();
+    }
+
+    return done(null, model.values);
+
+}).catch(e => console.log);
+
+const deserializeUser = (_id, done) => co(function* ()
+{
+    const model = yield UserModel.findOne({_id: new ObjectID(_id)});
+
+    if (!model)
+    {
+        return done();
+    }
+
+    return done(null, model.values);
+
+}).catch(e => console.log);
+
+passport.use(new LocalStrategy({usernameField: 'email'}, authHandler));
+
+passport.serializeUser((user, done) =>
+{
+    done(null, user._id);
+});
+
+passport.deserializeUser(deserializeUser);
+
+export default {
+
+    get passport ()
+    {
         return passport;
-    }
+    },
 
-    get localAuth () {
+    get localAuth ()
+    {
         return localAuth;
     }
 
-    _authHandler (email, password, done) {
-        return co(function* () {
-            const model = yield UserModel.findOne({email});
-
-            if (!model) {
-                return done();
-            }
-
-            if (!(yield model.checkPassword(password))) {
-                return done();
-            }
-
-            return done(null, model.values);
-
-        }.bind(this)).catch(e => console.log);
-    }
-
-    _deserializeUser (_id, done) {
-        return co(function* () {
-            const model = yield UserModel.findOne({_id: new ObjectID(_id)});
-
-            if (!model) {
-                return done();
-            }
-
-            return done(null, model.values);
-        }.bind(this)).catch((error) => {
-            done(error);
-        });
-    }
 }
-
-export default new PassportManager();
