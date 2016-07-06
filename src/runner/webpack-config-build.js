1849c0e7d723e994c77137fb7e8471b5431d8083
Ippei Tanaka <itanaka@unioncreative.com>
2016-07-06 10:19:20 -0400

    Refactor webpack.config

---
+++ src/runner/webpack-config-build.js
@@ -0,0 +1,100 @@
+import path from 'path';
+import webpack from 'webpack';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+
+export default ({
+    webpackServerHost = "localhost",
+    webpackServerPort = 8080,
+    envVariables = {},
+    envNamespace = 'WEBLOG_WEBPACK_ENV',
+    production = false,
+    sourceMap = false,
+    staticDir = path.resolve(__dirname, '../client/static'),
+    bundleDirName = "bundle",
+    adminEntryFile = path.resolve(__dirname, "../admin-app/browser-entry.js"),
+    publicEntryFile = path.resolve(__dirname, "../public-app/browser-entry.js"),
+    nodeModuleDir = path.resolve(__dirname, "../../node_modules"),
+    vendorFileName = "vendor.js",
+    cssFileName = "[name]-style.css"
+    }) =>
+{
+
+    const outputDir = path.resolve(staticDir, bundleDirName);
+
+    let plugins = [
+        new webpack.DefinePlugin({
+            "process.env": {
+                [envNamespace]: JSON.stringify(envVariables)
+            }
+        }),
+        new webpack.optimize.CommonsChunkPlugin(vendorFileName)
+    ];
+
+    if (production)
+    {
+        plugins = plugins.concat([
+            new ExtractTextPlugin(cssFileName, {allChunks: true}),
+            new webpack.DefinePlugin({
+                "process.env": {
+                    NODE_ENV: JSON.stringify("production")
+                }
+            }),
+            new webpack.optimize.UglifyJsPlugin({
+                minimize: true,
+                compress: {
+                    warnings: false
+                }
+            })
+        ]);
+    }
+
+    return {
+        entry: {
+            admin: [
+                "babel-polyfill",
+                adminEntryFile
+            ],
+            public: [
+                "babel-polyfill",
+                publicEntryFile
+            ]
+        },
+
+        output: {
+            path: outputDir,
+            filename: "[name].js",
+            publicPath: `http://${webpackServerHost}:${webpackServerPort}/${bundleDirName}`
+        },
+
+        devtool: sourceMap ? 'source-map' : null,
+
+        module: {
+            loaders: [
+                {
+                    test: /\.jsx?$/,
+                    loader: 'babel-loader',
+                    exclude: /node_modules/,
+                    query: {
+                        presets: ['es2015', 'react']
+                    }
+                },
+                {
+                    test: /\.scss$/,
+                    loader: production ? ExtractTextPlugin.extract("style", "css!sass") : null,
+                    loaders: production ? null : ["style", "css", "sass"]
+                }
+            ]
+        },
+
+        resolve: {
+            extensions: ['', '.js', '.jsx']
+        },
+
+        resolveLoader: {
+            root: nodeModuleDir
+        },
+
+        plugins: plugins
+
+    };
+}
--- src/runner/webpack-runner.js
+++ src/runner/webpack-runner.js
@@ -1,12 +1,25 @@
 import webpack from "webpack";
-import webpackConfig from './webpack.config';
+import webpackConfigBuild from './webpack-config-build';
+import { getEnv } from '../env-variables';
 
-export const run = () => new Promise((resolve, reject) => {
-    webpack(webpackConfig, (err, stats) => {
-        if (!err) {
-            console.log (stats.toString({colors: true}));
+export const run = () => new Promise((resolve, reject) =>
+{
+    const ENV = getEnv();
+
+    const config = webpackConfigBuild({
+        envVariables: ENV,
+        production: true
+    });
+
+    webpack(config, (err, stats) =>
+    {
+        if (!err)
+        {
+            console.log(stats.toString({colors: true}));
             resolve();
-        } else {
+        }
+        else
+        {
             reject(err);
         }
     });
