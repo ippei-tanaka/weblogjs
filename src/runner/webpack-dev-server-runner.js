1e508fd0db96410c16b983ed0d4122afe4919dbe
Ippei Tanaka <tanaka.ippei.jp@gmail.com>
2016-07-06 10:19:20 -0400

    Refactor webpack.config

--- src/runner/webpack-dev-server-runner.js
+++ src/runner/webpack-dev-server-runner.js
@@ -1,17 +1,29 @@
 import webpack from "webpack";
 import WebpackDevServer from 'webpack-dev-server';
-import webpackConfig from './webpack.config';
+import webpackConfigBuild from './webpack-config-build';
 import { getEnv } from '../env-variables';
 
-export const run = () => new Promise((resolve, reject) => {
-    const compiler = webpack(webpackConfig);
+export const run = () => new Promise((resolve, reject) =>
+{
+    const ENV = getEnv();
+
+    const config = webpackConfigBuild({
+        webpackServerHost: ENV.webpack_server_port,
+        webpackServerPort: ENV.webpack_server_host,
+        sourceMap: true,
+        envVariables: ENV
+    });
+
+    const compiler = webpack(config);
+
     const server = new WebpackDevServer(compiler, {
         inline: true,
         hot: true,
-        publicPath: webpackConfig.output.publicPath,
+        publicPath: config.output.publicPath,
         stats: {colors: true}
     });
-    const ENV = getEnv();
+
     server.listen(ENV.webpack_server_port, ENV.webpack_server_host);
+
     resolve();
 });
