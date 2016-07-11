48a361ff3c1b059080d879779a133e316e73b12a
Ippei Tanaka <itanaka@unioncreative.com>
2016-07-11 18:34:01 -0400

    Update development script

--- scripts/development.js
+++ scripts/development.js
@@ -1,4 +1,7 @@
 import WeblogJS from '../src/index';
+import co from 'co';
+
+console.log("Starting Development Servers...");
 
 const config = {
     webPort: 3001,
@@ -7,9 +10,16 @@ const config = {
 
 WeblogJS.setConfig(config);
 
-WeblogJS.startWebServer().then(() => {
+co(function* () {
+    yield WeblogJS.startWebServer();
+
+    const conf = WeblogJS.getConfig();
+    console.log("");
     console.log("Web Server started...");
-    WeblogJS.startBrowserEntryFileServer();
+    console.log(`Go to ${conf.webProtocol}://${conf.webHost}:${conf.webPort}`);
+
+    yield WeblogJS.startBrowserEntryFileServer();
+
 }).catch(e => {
     console.error(e);
 });
