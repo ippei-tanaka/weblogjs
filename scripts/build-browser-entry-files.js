4d7f505be11098e63ab3214e281ca82d9521c1bd
Ippei Tanaka <itanaka@unioncreative.com>
2016-07-13 12:11:54 -0400

    Let cli show log

--- scripts/build-browser-entry-files.js
+++ scripts/build-browser-entry-files.js
@@ -1,8 +1,6 @@
 import WeblogJS from '../src/index';
 import co from 'co';
 
-console.log("Building client entry files...");
-
 co(function* () {
     yield WeblogJS.buildBrowserEntryFiles();
 }).catch(e => {
