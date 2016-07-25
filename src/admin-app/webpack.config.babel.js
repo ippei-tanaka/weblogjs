f11b10ea4fd86549706f7af30c504c880c4ad938
Ippei Tanaka <tanaka.ippei.jp@gmail.com>
2016-07-25 09:48:36 -0400

    Update eslint json

--- src/admin-app/webpack.config.babel.js
+++ src/admin-app/webpack.config.babel.js
@@ -1,24 +1,15 @@
 import path from 'path';
 import webpack from 'webpack';
-//import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 const production = process.env.NODE_ENV === "production";
 
 let plugins = [
-    /*
-    new webpack.DefinePlugin({
-        "process.env": {
-            "ADMIN_": JSON.stringify(envVariables)
-        }
-    }),
-    */
     new webpack.optimize.CommonsChunkPlugin("vendor.js")
 ];
 
 if (production)
 {
     plugins = plugins.concat([
-        //new ExtractTextPlugin(cssFileName, {allChunks: true}),
         new webpack.DefinePlugin({
             "process.env": {
                 NODE_ENV: JSON.stringify("production")
@@ -43,8 +34,7 @@ export default {
 
     output: {
         path: path.resolve(__dirname, './bundle'),
-        filename: "[name].js",
-        //publicPath: `http://${webpackDevServerHost}:${webpackDevServerPort}/bundle`
+        filename: "[name].js"
     },
 
     devtool: production ? null : 'source-map',
@@ -59,16 +49,6 @@ export default {
                     presets: ['es2015', 'react']
                 }
             }
-            /*,
-            {
-                test: /\.scss$/,
-                loader: production ? ExtractTextPlugin.extract("style", "css!sass") : null,
-                loaders: production ? null : ["style", "css", "sass"]
-            },
-            {
-                test: /\.(eot|svg|ttf|woff(2)?)(\?v=\d+\.\d+\.\d+)?/,
-                loader: 'url-loader'
-            }*/
         ]
     },
 
